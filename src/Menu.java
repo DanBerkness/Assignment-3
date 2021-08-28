843eed473e56ca46027404d1c2051f0bfce9a955
DanBerkness <berknessdanielr@gmail.com>
2021-08-27 23:13:07 -0500

    Added Menu classes

---
+++ src/Menu.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+import com.sun.tools.javac.util.List;
+
+public class Menu {
+	private ArrayList<MenuOption> menuOptions;
+
+}
---
+++ src/MenuOption.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class MenuOption {
+	private String name;
+	private ArrayList<String> roles;
+}
