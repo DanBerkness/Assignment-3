19953ae189330eabebae1628f1bfa5eba790dd2e
Daniel Berkness <berknessdanielr@gmail.com>
2021-10-16 10:04:37 -0500

    Custom Exception

--- src/com/exceptions/UserNotFoundException.java
+++ src/com/exceptions/UserNotFoundException.java
@@ -3,5 +3,6 @@ package com.exceptions;
 public class UserNotFoundException extends Exception {
 	public UserNotFoundException(String errorMessage) {
 		super(errorMessage);
+//		Custom exception
 	}
 }
